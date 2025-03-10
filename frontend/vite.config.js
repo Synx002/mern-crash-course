c5ad5b46ebfe7532b847ee284029df24be414284
Mohammad Syam <mosyam0202@gmail.com>
2025-03-10 13:36:42 +0700

    fix(build): change Vite output directory to public for Vercel compatibility

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+  server:{
+    proxy:{
+      "/api":{
+        target:"http://localhost:5000",
       }
     }
-  },
-  build: {
-    outDir: 'public', // This should change the output directory
-    chunkSizeWarningLimit: 1000
   }
 })
