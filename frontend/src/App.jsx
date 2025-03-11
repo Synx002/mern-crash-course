bd0bdc328ed381dfbb3a57f0b88557fcca5d819b
Mohammad Syam <mosyam0202@gmail.com>
2025-03-11 12:55:47 +0700

    feat(router): add lazy loading for HomePage and CreatePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,29 @@
+import { lazy, Suspense } from "react";
 import { Box, useColorModeValue } from "@chakra-ui/react";
-import { Route, Routes } from 'react-router-dom';
-import HomePage from "./pages/HomePage.jsx";
-import CreatePage from "./pages/CreatePage.jsx";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 
+// Lazy Load Pages
+const HomePage = lazy(() => import("./pages/HomePage.jsx"));
+const CreatePage = lazy(() => import("./pages/CreatePage.jsx"));
+
 function App() {
+  const bgColor = useColorModeValue("gray.100", "gray.900");
+
   return (
-    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+    <Box minH="100vh" bg={bgColor}>
+      {/* Navbar */}
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-      </Routes>
+
+      {/* Routing dengan Suspense */}
+      <Suspense fallback={<Box p={4}>Loading...</Box>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </Suspense>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
